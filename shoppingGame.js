64b45e20897241ace5d115e17cdfb37d6a3d093d
DL217 <74790356+DL217@users.noreply.github.com>
2021-09-28 21:49:03 -0400

    Items can no longer be removed.

--- shoppingGame.js
+++ shoppingGame.js
@@ -1,17 +1,17 @@
 // Shopping List Game for Node.js
-// Draft #2 
-// Work in progress...
 
 const shoppingList = ["computer", "monitor", "keyboard", "mouse", "hdmi cable", "dvd drive"];
 let computerParts = [];
-let numberList = [];
 
 console.log("\nCongratulations! You are going shopping!")
 console.log("\nHere are the shopping items to choose from:")
 console.log() // Extra spaces were added to make the output more readable
+
 for (const [item, element] of shoppingList.entries()) {
   console.log(item + 1, element);
 }
+
+console.log("\nThe store is in limited supply! You may only check out 6 items.")
 
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -22,67 +22,10 @@ const rl = readline.createInterface({
 rl.question("Press 'Enter' to start.", (firstResponse) => {
     function itemSelection() {
         if (computerParts.length !== shoppingList.length) {
-            let currentChoice = rl.question ("\nAdd items to your cart by entering their corresponding number (one item at a time).", response => {
-                response = response.replace(/\s+/g, '');
-                response = parseInt(response);
-                if (response in numberList) {
-                    const index = shoppingList.indexOf("computer");
-                    console.log(index+"is the index.");
-                    computerParts.splice(index, 1);
-                    numberList.pop();
-                    console.log(numberList);
-                    console.log(`Removing ${response}. Your cart now contains ${computerParts}.`);
-                    itemSelection();
-                } else {
-                    switch (response) {
-                        case 1:
-                            computerParts.push("computer");
-                            numberList.push(1);
-                            console.log(numberList);
-                            console.log(`Adding ${response}. Your cart now contains: ${computerParts}.\nPlease select another item.`);
-                            itemSelection();
-                            break;
-                        case 2:
-                            computerParts.push("monitor");
-                            numberList.push(2);
-                            console.log(numberList);
-                            console.log(`Adding ${response}. Your cart now contains: ${computerParts}.\nPlease select another item.`);
-                            itemSelection();
-                            break;
-                        case 3:
-                            computerParts.push("keyboard");
-                            numberList.push(3);
-                            console.log(numberList);
-                            console.log(`Adding ${response}. Your cart now contains: ${computerParts}.\nPlease select another item.`);
-                            itemSelection();
-                            break;
-                        case 4:
-                            computerParts.push("mouse");
-                            numberList.push(4);
-                            console.log(numberList);
-                            console.log(`Adding ${response}. Your cart now contains: ${computerParts}.\nPlease select another item.`);
-                            itemSelection();
-                            break;
-                        case 5:
-                            computerParts.push("hdmi cable");
-                            numberList.push(5);
-                            console.log(numberList);
-                            console.log(`Adding ${response}. Your cart now contains: ${computerParts}.\nPlease select another item.`);
-                            itemSelection();
-                            break;
-                        case 6:
-                            computerParts.push("dvd drive");
-                            numberList.push(6);
-                            console.log(numberList);
-                            console.log(`Adding ${response}. Your cart now contains: ${computerParts}.\nPlease select another item.`);
-                            itemSelection();
-                            break;
-                        default:
-                            console.log("Please enter a valid choice.");
-                            itemSelection();
-                            break;
-                        }
-                    }
+            rl.question ("\nAdd items to your cart by entering their corresponding number (one item at a time).", response => {
+                computerParts.push(shoppingList[response - 1]);
+                console.log(`Adding ${response}. Your cart now contains: ${computerParts}.\nPlease select up to 6 items.`);
+                itemSelection();
             });
         } else {
             console.log("Your cart is full. Thank you for shopping!");
